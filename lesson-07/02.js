7d63df2c1a5adf028a2a61923978f3d32d0307b1
kirill poznyak <k.paznyak@gmail.com>
2025-03-08 22:03:14 +0300

    complete task 02 in hw7 autocheck

---
+++ lesson-07/02.js
@@ -0,0 +1,20 @@
+/*
+ * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
+ * Если строка является числом, функция должна возвращать true, в противном случае - false.
+ */
+
+function isNumeric(str) {
+  let result;
+  if (str === ' ') {
+    result = false;
+  } else {
+    result = isNaN(str) ? false : true;
+  }
+  return result;
+}
+
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
